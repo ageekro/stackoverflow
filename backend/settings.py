56a0cd6351e20e01e333254d92b8925035ef8558
Amirhosein Roshanaei <amirhoseinroshanaei12@gmail.com>
2020-02-18 17:36:53 +0330

    change settings file

--- backend/settings.py
+++ backend/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-o^4muqjh@lj%j865b_h3mxsn5$3mk7_isd*y3)p$)*c&%&qxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
