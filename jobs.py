94cc510b24cad3c089872fcb2487c8fb377badca
Amirhosein Roshanaei <amirhoseinroshanaei12@gmail.com>
2020-02-28 11:38:20 +0330

    change running time job to 1 day

--- jobs.py
+++ jobs.py
@@ -6,10 +6,10 @@ from qa.models import Question, QuestionViews
 
 def count_question_views():
     settings.configure()
-    time = timezone.now() - timedelta(hours=24)
+    time = timezone.now() - timedelta(days=1)
     question = Question()
     question_views = QuestionViews()
-    qs = question_views.collection.find({"created_at": {"$gt": time}})
+    qs = question_views.collection.find({"created_at": {"$gte": time}})
     for qv in qs:
         question_id = qv.get("question_id")
         question.collection.update_one({"_id": question_id}, {"$inc": {"views": 1}})
