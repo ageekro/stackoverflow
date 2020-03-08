c254b61450428df6a6bb5dbb4813144b3f292594
Amirhosein Roshanaei <amirhoseinroshanaei12@gmail.com>
2020-03-08 10:45:44 +0330

    add question and answer vote api urls

--- qa/api/urls.py
+++ qa/api/urls.py
@@ -5,11 +5,15 @@ from .views import (QuestionCreateAPIView,
                     QuestionDetailAPIView,
                     QuestionUpdateAPIView,
                     QuestionDestroyAPIView,
+                    QuestionVoteUpAPIView,
+                    QuestionVoteDownAPIView,
                     AnswerCreateAPIView,
                     AnswerListAPIView,
                     AnswerDetailAPIView,
                     AnswerUpdateAPIView,
-                    AnswerDestroyAPIView
+                    AnswerDestroyAPIView,
+                    AnswerVoteUpAPIView,
+                    AnswerVoteDownAPIView
                     )
 
 app_name = 'qa'
@@ -19,9 +23,13 @@ urlpatterns = [
     re_path(r'^questions/(?P<pk>[\w\d-]+)/$', QuestionDetailAPIView.as_view(), name='question-detail'),
     re_path(r'^questions/(?P<pk>[\w\d-]+)/update/$', QuestionUpdateAPIView.as_view(), name='question-update'),
     re_path(r'^questions/(?P<pk>[\w\d-]+)/delete/$', QuestionDestroyAPIView.as_view(), name='question-delete'),
+    re_path(r'^questions/(?P<pk>[\w\d-]+)/vote/up/$', QuestionVoteUpAPIView.as_view(), name='question-vote-up'),
+    re_path(r'^questions/(?P<pk>[\w\d-]+)/vote/down/$', QuestionVoteDownAPIView.as_view(), name='question-vote-down'),
     re_path(r'^answer/(?P<pk>[\w\d-]+)/$', AnswerCreateAPIView.as_view(), name="answer-create"),
     path('answers/', AnswerListAPIView.as_view(), name='answer-list'),
     re_path(r'^answers/(?P<pk>[\w\d-]+)/$', AnswerDetailAPIView.as_view(), name='answer-detail'),
     re_path(r'^answers/(?P<pk>[\w\d-]+)/update/$', AnswerUpdateAPIView.as_view(), name='answer-update'),
     re_path(r'^answers/(?P<pk>[\w\d-]+)/delete/$', AnswerDestroyAPIView.as_view(), name='answer-delete'),
+    re_path(r'^answers/(?P<pk>[\w\d-]+)/vote/up/$', AnswerVoteUpAPIView.as_view(), name='answer-vote-up'),
+    re_path(r'^answers/(?P<pk>[\w\d-]+)/vote/down/$', AnswerVoteDownAPIView.as_view(), name='answer-vote-down'),
 ]
